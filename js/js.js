e1e6e1ee1c9ff1b948a26b69c000869181dc0b8e
AlexMorro55 <alexmorro@paucasesnovescifp.cat>
2026-03-20 08:52:01 +0100

    js terminado

---
+++ js/js.js
@@ -0,0 +1,5 @@
+function enseñarConfirmacion(){
+    document.querySelector(".formulario").style.display = "none";
+    document.querySelector(".confirmacion").style.display = "flex";
+    document.querySelector(".confirmacion").style.animation = "aparecer 0.5s ease-in-out";
+}
